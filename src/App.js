4b3a08149f8135e0099bfc030d661a56dc4eaec3
AslinurTolga <aslinurtolga@gmail.com>
2022-10-25 18:42:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import Card from "./components/Card";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import RingLoader from "react-spinners/RingLoader";
+
+const url = "https://randomuser.me/api/";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+
+  const getUser = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setUser(data.results[0]);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+
+      setLoading(true);
+    }
+  };
+  console.log(user);
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (loading) {
+    return <RingLoader color="#EA5C2B" />;
+  }
+
+  return (
+    <div className="appcont">
+      <Card user={user} />
+      <button className="btn" onClick={() => getUser()}>
+        Random User
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,66 @@
+import "./Card.css";
+
+import emaillogo from "../assets/email.svg";
+import locationlogo from "../assets/location.svg";
+import phonelogo from "../assets/phone.svg";
+
+const Card = ({ user }) => {
+  const {
+    picture: { medium },
+    name: { title, first, last },
+    email,
+    cell,
+    location: { city, country },
+    dob: { age },
+    registered: { date },
+  } = user;
+
+  return (
+    <div className="bigContainer">
+      <div className="cardContainer">
+        <div>
+          <img src={medium} alt="img" />
+        </div>
+        <div>
+          <h3>
+            <span>{title} </span>
+            <span>{first} </span>
+            <span>{last}</span>
+          </h3>
+        </div>
+        <div>
+          {" "}
+          <img style={{ width: "40px" }} src={emaillogo} alt="" />
+        </div>
+        <div>
+          <h3>{email}</h3>
+        </div>
+        <div>
+          {" "}
+          <img style={{ width: "40px" }} src={phonelogo} alt="" />
+        </div>
+        <div>
+          <h3>{cell}</h3>
+        </div>
+        <div>
+          {" "}
+          <img style={{ width: "40px" }} src={locationlogo} alt="" />
+        </div>
+        <div>
+          <h3>
+            <span>{city} / </span>
+            <span>{country}</span>
+          </h3>
+        </div>
+        <div className="block">
+          <h3>Age : {age}</h3>
+        </div>
+        <div className="block">
+          <h3>Register Date : {date.substring(0, 10)}</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
